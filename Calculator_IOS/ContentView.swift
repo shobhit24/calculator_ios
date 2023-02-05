3f69f463aff6326babd1eced5501ed90a47c8e33
Shobhit Goswami <shobhit.goswami@zestmoney.in>
2023-02-06 00:21:16 +0530

    Completed calculator basic layout

--- Calculator_IOS/ContentView.swift
+++ Calculator_IOS/ContentView.swift
@@ -8,9 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttonGrid = [
+        ["AC", "⌫", "%", "/"],
+        ["7", "8", "9", "x"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        [".", "0", "", "="]
+    ]
+    
+    let operators = ["/", "+", "x", "%"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                
+            Spacer()
+                
+            Text("History")
+                .padding()
+                .foregroundColor(.white)
+                .font(.system(size: 30, weight: .heavy))
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            HStack {
+                
+                Spacer()
+                    
+                Text("Current")
+                    .padding()
+                    .foregroundColor(.white)
+                    .font(.system(size: 50, weight: .heavy))
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+                ForEach(buttonGrid, id: \.self) {
+                    row in
+                    HStack{
+                        ForEach(row, id: \.self) {
+                            cell in
+                            
+                            Button(
+                                action: {
+                                buttonPressed(cell: cell)
+                                }, label: {
+                                Text(cell)
+                                    .foregroundColor(buttonColor(cell))
+                                    .font(.system(size: 50, weight: .heavy))
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                }
+                            )
+                        }
+                    }
+                    
+                }
+        }
+        .background(Color.black)
+    }
+    
+    func buttonColor(_ cell: String) -> Color {
+        if(cell == "AC" || cell == "⌫"){
+            return .red
+            
+        }
+        if(cell == "-" || cell == "=" || operators.contains(cell)){
+            return .orange
+            
+        }
+        return .white
+    }
+    
+    func buttonPressed(cell: String) {
+        
     }
 }
 
