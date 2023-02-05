c567fbe364655d398dafd316364c8a571bfa62dd
Shobhit Goswami <shobhit.goswami@zestmoney.in>
2023-02-05 23:08:38 +0530

    Initial Commit

---
+++ Calculator_IOS/Calculator_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_IOSApp.swift
+//  Calculator_IOS
+//
+//  Created by Shobhit Goswami on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator_IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator_IOS
+//
+//  Created by Shobhit Goswami on 05/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
