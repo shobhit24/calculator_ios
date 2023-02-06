32a1117ff51ac1791f59b8333c614584929116ee
Shobhit Goswami <shobhit.goswami@zestmoney.in>
2023-02-06 14:52:23 +0530

    UI Fixes - Changed layout and button's height and width

--- Calculator_IOS/ContentView.swift
+++ Calculator_IOS/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
         ["7", "8", "9", "x"],
         ["4", "5", "6", "-"],
         ["1", "2", "3", "+"],
-        [".", "0", "", "="]
+        [".", "0", "="]
     ]
     
     let operators = ["/", "+", "x", "%"]
@@ -27,7 +27,7 @@ struct ContentView: View {
                 
             Text("History")
                 .padding()
-                .foregroundColor(.white)
+                .foregroundColor(.black)
                 .font(.system(size: 30, weight: .heavy))
             }.frame(maxWidth: .infinity, maxHeight: .infinity)
             
@@ -37,32 +37,39 @@ struct ContentView: View {
                     
                 Text("Current")
                     .padding()
-                    .foregroundColor(.white)
+                    .foregroundColor(.black)
                     .font(.system(size: 50, weight: .heavy))
                 }.frame(maxWidth: .infinity, maxHeight: .infinity)
             
-                ForEach(buttonGrid, id: \.self) {
-                    row in
-                    HStack{
-                        ForEach(row, id: \.self) {
-                            cell in
-                            
-                            Button(
-                                action: {
-                                buttonPressed(cell: cell)
-                                }, label: {
-                                Text(cell)
-                                    .foregroundColor(buttonColor(cell))
-                                    .font(.system(size: 50, weight: .heavy))
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                }
-                            )
-                        }
+            ForEach(buttonGrid, id: \.self) { row in
+                HStack(alignment: .top,spacing: 12) {
+                    ForEach(row, id: \.self) { cell in
+                        if(cell != ""){ Button(action: {
+                            buttonPressed(cell: cell)
+                        }, label: {
+                            Text(cell)
+                                .font(.system(size: 24))
+                                .frame(width: self.buttonWidth(cell),
+                                       height: self.buttonHeight(cell))
+                                .foregroundColor(.black)
+                                .overlay( cell == "=" ?
+                                          RoundedRectangle(cornerRadius: 42).stroke(.white, lineWidth: 3) :
+                                            RoundedRectangle(cornerRadius: 70).stroke(.white, lineWidth: 3)
+                                    )
+
+
+                        })}
+                       
                     }
-                    
                 }
+                .shadow(color: .gray, radius: 1, x: 1, y: 2)
+                .padding(.bottom, 3)
+                Spacer()
+            }
+            
+            
         }
-        .background(Color.black)
+        .background(Color.white)
     }
     
     func buttonColor(_ cell: String) -> Color {
@@ -75,6 +82,17 @@ struct ContentView: View {
             
         }
         return .white
+    }
+    
+    func buttonWidth(_ cell: String) -> CGFloat {
+        if cell == "=" {
+            return ((UIScreen.main.bounds.width + 60 - (4*12)) / 5) * 2
+        }
+        return (UIScreen.main.bounds.width + 60 - (5*12)) / 5
+    }
+    
+    func buttonHeight(_ cell: String) -> CGFloat {
+        return (UIScreen.main.bounds.width - 60 - (5*12)) / 4
     }
     
     func buttonPressed(cell: String) {
